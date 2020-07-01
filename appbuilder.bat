bef1306a1ed37f0d6017e4a5f4344307226dee59
E-compfast Digital Indonesia <58408252+e-compfast@users.noreply.github.com>
2020-07-01 10:01:50 +0700

    Update appbuilder.bat

--- appbuilder.bat
+++ appbuilder.bat
@@ -49,70 +49,7 @@ IF %program% == b cordova plugin add cordova-plugin-admobpro --save --variable A
 IF %program% == c cordova plugin add cordova-plugin-app-name --variable APP_NAME="Master Chess Indonesia"
 IF %program% == d cordova build android
 IF %program% == e cordova build android --release
-IF %program% == f cd platforms\android\app\build\outputs\apk\release && -keyalg RSA -keysize 2048 -validity 10000 && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore namakeystore.keystore app-release-unsigned.apk namaAplikasi && zipalign -v 4 app-release-unsigned.apk jelly-release1.0.0.apk
-IF %program% == g cordova build android
-
-IF %program% == 1 cordova platform add android
-IF %program% == 2 cordova platform add ios
-IF %program% == 3 cordova platform add browser
-
-:skip
-ECHO + Program berhenti! Jalankan ulang? (y/n)
-ECHO.
-ECHO.
- 
-@ECHO OFF
-@CLS
-
-ECHO.
-ECHO.	===========================================================
-ECHO  	 TERIMAKASIH! TELAH MEMBELI SOURCE CODE DI UNITSELLERADS01
-ECHO.	===========================================================
-
-ECHO.
-ECHO.
-
-ECHO.	===========================================================
-ECHO  	 Daftar Pilihan Program :
-ECHO.	===========================================================
-ECHO.
-rem ECHO  	[a] Membuat Project Aplikasi
-ECHO  	[b] Menambahkan Admob Plugin
-ECHO  	[c] Menambahkan App Name Plugin
-ECHO.
-ECHO  	[d] Membuat Testing Aplikasi [ Debugging ]
-ECHO  	[e] Membuat Aplikasi Production [ Rilis ]
-ECHO  	[f] Membuat Aplikasi Siap Upload Google Play
-ECHO.
-ECHO  	[g] Update aplikasi 
-ECHO.
-ECHO.	===========================================================
-
-ECHO.
-ECHO.
-
-ECHO.	===========================================================
-ECHO  	 Silahkan pilih platform yang kamu inginkan :
-ECHO.	===========================================================
-ECHO.
-ECHO  	[1] Android
-ECHO  	[2] IOS
-ECHO  	[3] Web
-ECHO.
-ECHO.	===========================================================
-
-ECHO.
-ECHO.
-		SET /p "program= + Masukan pilihan program/platform kamu : "
-ECHO.
-ECHO.
-
-rem IF %program% == a cordova create masterchess com.masterchess.app masterChess
-IF %program% == b cordova plugin add cordova-plugin-admobpro --save --variable ADMOB_APP_ID="ca-app-pub-3821963146527465~9690679929"
-IF %program% == c cordova plugin add cordova-plugin-app-name --variable APP_NAME="Master Chess Indonesia"
-IF %program% == d cordova build android
-IF %program% == e cordova build android --release
-IF %program% == f cd platforms\android\app\build\outputs\apk\release && -keyalg RSA -keysize 2048 -validity 10000 && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore namakeystore.keystore app-release-unsigned.apk namaAplikasi && zipalign -v 4 app-release-unsigned.apk app-release1.0.0.apk
+IF %program% == f cd platforms\android\app\build\outputs\apk\release
 IF %program% == g cordova build android
 
 IF %program% == 1 cordova platform add android
